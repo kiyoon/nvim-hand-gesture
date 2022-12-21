718631c572683b21ed9f90e8d5b46fb1e99b109e
Kiyoon Kim <yoonkr33@gmail.com>
2022-12-21 00:42:40 +0000

    normal mode nvim tree

--- gesture_mappings/swiping_left.lua
+++ gesture_mappings/swiping_left.lua
@@ -1,5 +1,6 @@
 local mode = vim.api.nvim_get_mode().mode
 if mode == 'n' then
+  vim.api.nvim_command('NvimTreeClose')
 elseif mode == 'i' then
   vim.cmd[[call copilot#Previous()]]
 end
--- gesture_mappings/swiping_right.lua
+++ gesture_mappings/swiping_right.lua
@@ -1,5 +1,10 @@
 local mode = vim.api.nvim_get_mode().mode
 if mode == 'n' then
+  local buf = vim.api.nvim_get_current_buf()
+  -- open nvim tree
+  vim.api.nvim_command('NvimTreeOpen')
+  -- focus on the original buffer (uninterrupted workflow)
+  vim.api.nvim_set_current_buf(buf)
 elseif mode == 'i' then
   vim.cmd[[call copilot#Next()]]
 end
